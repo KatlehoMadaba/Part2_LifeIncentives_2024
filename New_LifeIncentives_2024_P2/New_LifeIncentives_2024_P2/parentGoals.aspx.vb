7222e5ad2cb8597be2d1741991038ef9120b42ca
ibrahim2k03 <ibrahimkabore1108@gmail.com>
2024-01-28 23:43:24 +0200

    Parent goals outline
    Components for parent goals

--- New_LifeIncentives_2024_P2/New_LifeIncentives_2024_P2/parentGoals.aspx.vb
+++ New_LifeIncentives_2024_P2/New_LifeIncentives_2024_P2/parentGoals.aspx.vb
@@ -1,8 +1,71 @@
-﻿Public Class parentGoals
+﻿Imports System.Data.SqlClient
+
+
+
+Public Class TaskItem
+    Public Property Task As String
+    Public Property Price As Integer
+End Class
+
+
+
+Partial Public Class parentGoals
     Inherits System.Web.UI.Page
 
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+
+
 
     End Sub
 
+
+
+
+    Protected Sub Add()
+
+        Dim sg As New TBL_Set_Goals
+        DBInterface.connect("conn")
+
+
+        sg.Category = DropDownList1.SelectedValue
+        sg.StartDateTime = DateTime.Now
+        sg.Description = "Task"
+        sg.Amount = 34
+        sg.TimePeriod = 23
+        sg.EndDateTime = DateTime.Now
+        sg.Status = "Incomplete"
+        sg.update()
+
+
+    End Sub
+
+
+    Protected Sub btnSubmit_Click1(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
+
+
+
+    Private Sub SaveSelectedItemToDatabase(ByVal description As String, ByVal price As Integer, ByVal I_Category As String, ByVal Category As String)
+        Dim Child_IDNo As String = "123"
+
+
+    End Sub
+
+    Protected Sub DropDownList1_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
+
+
+
+
+
+
+
+
+
 End Class
+
